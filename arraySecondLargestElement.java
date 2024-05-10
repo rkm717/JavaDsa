1c62e5751763b89dd80b0e95058a3f1019ab7a2e
rkm717 <rkmishra717@gmail.com>
2024-05-10 10:02:19 +0530

    add code

---
+++ arraySecondLargestElement.java
@@ -0,0 +1,27 @@
+class secondLargest {
+    static int secondLargestarray(int[]a){
+        int max1=0,max2=0;
+        if(a[0] > a[1]){
+            max1 = a[0];
+            max2 = a[1];
+        }else {
+            max1 = a[1];
+            max2 = a[0];
+        }
+        for(int i=2; i<a.length; i++){
+            if(a[i] > max1){
+                max2 = max1;
+                max1 = a[i];
+
+            }else if(a[i] > max2){
+               
+             max2 = a[i];   
+            }
+        }
+        return max2;
+    }
+	public static void main(String args[]) {
+		int[] a = {20,42,6,25,48,88};
+        System.out.println(secondLargestarray(a)); 
+	}
+}
